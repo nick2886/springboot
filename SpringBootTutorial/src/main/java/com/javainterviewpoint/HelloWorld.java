4c20e4b1a66752c7bf36eaf05f5ac010d0d838f4
Arora <nikhil.arora@capgemini.com>
2020-07-18 20:57:28 +0530

    main issue resolved for App tut

--- SpringBootTutorial/src/main/java/com/javainterviewpoint/HelloWorld.java
+++ SpringBootTutorial/src/main/java/com/javainterviewpoint/HelloWorld.java
@@ -1,6 +1,5 @@
 package com.javainterviewpoint;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +10,7 @@ public class HelloWorld {
 
     @RequestMapping("/")
     String hello() {
-        return "Hello World! JavaInterviewPoint";
+        return "Hello Dear World! JavaInterviewPoint";
     }
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(HelloWorld.class, args);
-    }
 }
